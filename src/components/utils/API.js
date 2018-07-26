9b27b1883114dc092747833da3e7802de540dbcc
Justin <jwhittington92@yahoo.com>
2018-07-26 00:10:14 -0400

    listing into db

--- src/components/utils/API.js
+++ src/components/utils/API.js
@@ -1,17 +1,27 @@
-import axios from "axios";
+import axios from 'axios';
 
 export default {
-  // Gets all books
-  getUser: function(username) {
-      console.log("here API")
-    return axios.get("/user/" + username);
-  },
-  sendMessage: function(receiverData) {
-      console.log("message API")
-      return axios.post("/user/send", receiverData)
-  },
-  getMessageBody: function(id) {
-      console.log("here API body" + id);
-      return axios.get("/message/" + id);
-  }
+	// Gets all books
+	getUser: function(username) {
+		console.log('here API');
+		return axios.get('/user/' + username);
+	},
+	sendMessage: function(receiverData) {
+		console.log('message API');
+		return axios.post('/user/send', receiverData);
+	},
+	getMessageBody: function(id) {
+		console.log('here API body' + id);
+		return axios.get('/message/' + id);
+	},
+///////////////////////////////////////////////////////////////////
+    // Listing
+    getListings: function() {
+        console.log('hit browse api')
+        return axios.get("/browse/");
+      },
+	saveListing: function(listingData) {
+		console.log('hit listing api', listingData);
+		return axios.post('/listing/', listingData);
+	}
 };
